90797005aa28d90fc13846ad8584f071977db431
cbdyzj <cbdyzj@jianzhao.org>
2021-03-05 20:49:51 +0800

    Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function Carbon({ Component, pageProps }) {
+    return <Component {...pageProps} />
 }
-
-export default MyApp
